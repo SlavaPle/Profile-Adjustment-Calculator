e634985dcadef6a89d5528284f3860ddf159c5b3
SlavaPle <125448880+SlavaPle@users.noreply.github.com>
2023-08-01 22:39:33 +0200

    Добавьте файлы проекта.

---
+++ Profile Adjustment Calculator/Form1.Designer.cs
@@ -0,0 +1,310 @@
+﻿namespace Profile_Adjustment_Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NudStandard = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.NudMin = new System.Windows.Forms.NumericUpDown();
+            this.CbMiddle = new System.Windows.Forms.CheckBox();
+            this.CbPrecision = new System.Windows.Forms.CheckBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.TbDeviation = new System.Windows.Forms.TrackBar();
+            this.label8 = new System.Windows.Forms.Label();
+            this.NudMax = new System.Windows.Forms.NumericUpDown();
+            this.CbReverse = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.NudActual = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.NudStandard)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NudMin)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TbDeviation)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NudMax)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NudActual)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // NudStandard
+            // 
+            this.NudStandard.Location = new System.Drawing.Point(12, 48);
+            this.NudStandard.Maximum = new decimal(new int[] {
+            3000,
+            0,
+            0,
+            0});
+            this.NudStandard.Name = "NudStandard";
+            this.NudStandard.Size = new System.Drawing.Size(151, 20);
+            this.NudStandard.TabIndex = 0;
+            this.NudStandard.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.NudStandard.Value = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.NudStandard.ValueChanged += new System.EventHandler(this.EnterData_Changed);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Standard value";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Min value";
+            // 
+            // NudMin
+            // 
+            this.NudMin.Location = new System.Drawing.Point(12, 87);
+            this.NudMin.Maximum = new decimal(new int[] {
+            3000,
+            0,
+            0,
+            0});
+            this.NudMin.Name = "NudMin";
+            this.NudMin.Size = new System.Drawing.Size(72, 20);
+            this.NudMin.TabIndex = 2;
+            this.NudMin.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.NudMin.Value = new decimal(new int[] {
+            995,
+            0,
+            0,
+            0});
+            this.NudMin.ValueChanged += new System.EventHandler(this.EnterData_Changed);
+            // 
+            // CbMiddle
+            // 
+            this.CbMiddle.AutoSize = true;
+            this.CbMiddle.Location = new System.Drawing.Point(12, 179);
+            this.CbMiddle.Name = "CbMiddle";
+            this.CbMiddle.Size = new System.Drawing.Size(105, 17);
+            this.CbMiddle.TabIndex = 7;
+            this.CbMiddle.Text = "Middle the range";
+            this.CbMiddle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.CbMiddle.UseVisualStyleBackColor = true;
+            this.CbMiddle.CheckedChanged += new System.EventHandler(this.EnterData_Changed);
+            // 
+            // CbPrecision
+            // 
+            this.CbPrecision.AutoSize = true;
+            this.CbPrecision.Location = new System.Drawing.Point(12, 202);
+            this.CbPrecision.Name = "CbPrecision";
+            this.CbPrecision.Size = new System.Drawing.Size(135, 17);
+            this.CbPrecision.TabIndex = 8;
+            this.CbPrecision.Text = "Precision measurement";
+            this.CbPrecision.UseVisualStyleBackColor = true;
+            this.CbPrecision.CheckedChanged += new System.EventHandler(this.EnterData_Changed);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(194, 80);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(594, 527);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 19;
+            this.pictureBox1.TabStop = false;
+            // 
+            // TbDeviation
+            // 
+            this.TbDeviation.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TbDeviation.Location = new System.Drawing.Point(194, 29);
+            this.TbDeviation.Maximum = 30;
+            this.TbDeviation.Minimum = -30;
+            this.TbDeviation.Name = "TbDeviation";
+            this.TbDeviation.Size = new System.Drawing.Size(594, 45);
+            this.TbDeviation.TabIndex = 20;
+            this.TbDeviation.ValueChanged += new System.EventHandler(this.EnterData_Changed);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(91, 71);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(56, 13);
+            this.label8.TabIndex = 22;
+            this.label8.Text = "Max value";
+            // 
+            // NudMax
+            // 
+            this.NudMax.Location = new System.Drawing.Point(91, 87);
+            this.NudMax.Maximum = new decimal(new int[] {
+            3000,
+            0,
+            0,
+            0});
+            this.NudMax.Name = "NudMax";
+            this.NudMax.Size = new System.Drawing.Size(72, 20);
+            this.NudMax.TabIndex = 21;
+            this.NudMax.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.NudMax.Value = new decimal(new int[] {
+            1005,
+            0,
+            0,
+            0});
+            this.NudMax.ValueChanged += new System.EventHandler(this.EnterData_Changed);
+            // 
+            // CbReverse
+            // 
+            this.CbReverse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.CbReverse.AutoSize = true;
+            this.CbReverse.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.CbReverse.Location = new System.Drawing.Point(722, 6);
+            this.CbReverse.Name = "CbReverse";
+            this.CbReverse.Size = new System.Drawing.Size(66, 17);
+            this.CbReverse.TabIndex = 23;
+            this.CbReverse.Text = "Reverse";
+            this.CbReverse.UseVisualStyleBackColor = true;
+            this.CbReverse.CheckedChanged += new System.EventHandler(this.EnterData_Changed);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 23);
+            this.button1.TabIndex = 24;
+            this.button1.Text = "Relative";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(12, 129);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            3000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(151, 20);
+            this.numericUpDown1.TabIndex = 0;
+            this.numericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.EnterData_Changed);
+            // 
+            // NudActual
+            // 
+            this.NudActual.Location = new System.Drawing.Point(12, 129);
+            this.NudActual.Maximum = new decimal(new int[] {
+            3000,
+            0,
+            0,
+            0});
+            this.NudActual.Name = "NudActual";
+            this.NudActual.Size = new System.Drawing.Size(151, 20);
+            this.NudActual.TabIndex = 0;
+            this.NudActual.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.NudActual.Value = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.NudActual.ValueChanged += new System.EventHandler(this.EnterData_Changed);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 113);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(66, 13);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Actual value";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 614);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.CbReverse);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.NudMax);
+            this.Controls.Add(this.TbDeviation);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.CbPrecision);
+            this.Controls.Add(this.CbMiddle);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.NudMin);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.NudActual);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.NudStandard);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.NudStandard)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NudMin)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TbDeviation)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NudMax)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NudActual)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown NudStandard;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown NudMin;
+        private System.Windows.Forms.CheckBox CbMiddle;
+        private System.Windows.Forms.CheckBox CbPrecision;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TrackBar TbDeviation;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.NumericUpDown NudMax;
+        private System.Windows.Forms.CheckBox CbReverse;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown NudActual;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Profile Adjustment Calculator/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Profile_Adjustment_Calculator
+{
+
+    public partial class Form1 : Form
+    {
+        Profile P;
+        CabinSide S;
+        bool Absolute = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            P = new Profile();
+            S = new CabinSide();
+            EnterData_Changed(sender, e);
+            NudMax.Minimum = NudStandard.Value;
+            NudMin.Maximum = NudStandard.Value;
+            NudMax.Maximum = NudStandard.Value + 30;
+            NudMax.Minimum = NudStandard.Value - 30;
+        }
+
+        bool Do = true;
+        private void EnterData_Changed(object sender, EventArgs e)
+        {
+            if (Do)
+            {
+                int St = Absolute ? 0 : Convert.ToInt32(NudStandard.Value);
+                int Min = (Absolute ? -1 : 1) * Convert.ToInt32(NudMin.Value);
+                int Max = Convert.ToInt32(NudMax.Value);
+                int mid = 0;
+                P.Adjustment = Max - Min;
+                P.UpAdjustment = CbMiddle.Checked ? Convert.ToInt32(Math.Round(P.Adjustment / 2f + .1)) : Max - St;
+                P.DownAdjustment = CbMiddle.Checked ? -Convert.ToInt32(Math.Round(P.Adjustment / 2f - .1)) : Min - St;
+                mid = Min - St - P.DownAdjustment;
+                if (CbMiddle.Checked) St += mid;
+                S.Deviation = TbDeviation.Value;
+                S.Precision = CbPrecision.Checked;
+                S.ActualValue = Convert.ToInt32(NudActual.Value) + (CbMiddle.Checked ? mid : 0);
+                S.Reverse = CbReverse.Checked;
+                S.Count(P);
+                pictureBox1.Image = S.Img;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Do = false;
+            Absolute = !Absolute;
+            if (Absolute)
+            {
+                button1.Text = "Absolute";
+                NudStandard.Visible = false;
+                label1.Visible = false;
+                NudMax.Minimum = 0;
+                NudMin.Minimum = 0;
+                NudMax.Value = NudMax.Value - NudStandard.Value;
+                NudMin.Value = NudStandard.Value - NudMin.Value;
+                NudMax.Maximum = 30;
+                NudMin.Maximum = 30;
+            }
+            else
+            {
+                button1.Text = "Relative";
+                NudStandard.Visible = true;
+                label1.Visible = true;
+                NudMin.Minimum = 0;
+                NudMax.Maximum = NudStandard.Value + 30;
+                NudMin.Maximum = NudStandard.Value;
+                NudMax.Value = NudMax.Value + NudStandard.Value;
+                NudMin.Value = NudStandard.Value - NudMin.Value;
+                NudMax.Minimum = NudStandard.Value;
+            }
+            Do = true;
+            EnterData_Changed(sender, e);
+        }
+    }
+
+    public class Profile
+    {
+        public int Adjustment { get; set; }
+        public int UpAdjustment { get; set; }
+        public int DownAdjustment { get; set; }
+    }
+
+    class CabinSide
+    {
+        public bool Reverse { get; set; }
+        public int Deviation { get; set; }
+        public bool Precision { get; set; }
+        public bool Middle { get; set; }
+        public bool Tilt { get; private set; }
+        public int TiltValue { get; private set; }
+        public int Value { get; private set; }
+        public int ActualValue { get; set; }
+        public Image Img { get; private set; }
+
+        public void Count(Profile P)
+        {
+            int MainAdjustment = Math.Abs(Deviation > 0 ? P.UpAdjustment : P.DownAdjustment);
+            Tilt = Math.Abs(Deviation) + (Precision ? 0 : 1) >= P.Adjustment ? true : false;
+            TiltValue = Tilt ? Deviation + Math.Sign(Deviation) * ((Precision ? 0 : 2) - P.Adjustment) : 0;
+            Value = (Math.Abs(Deviation) < MainAdjustment + (Precision ? 1 : 0)) || Deviation == 0
+                ? 0 :
+                Math.Abs(Deviation) < P.Adjustment + (Precision ? 1 : 0) ?
+              Convert.ToInt32(Math.Round((Deviation - P.Adjustment) / 2f - Math.Sign(Deviation) * .1) + Math.Abs(P.DownAdjustment)) :
+              Deviation < 0 ? (Precision ? 0 : 1) - P.UpAdjustment : Math.Abs(P.DownAdjustment) - (Precision ? 0 : 1);
+            Draw(P.Adjustment, P.DownAdjustment);
+        }
+
+        void Draw(int Adj, int Min)
+        {
+            int k = 30;
+            int W = 30 * k;
+            int H = 50 * k;
+            int zero = 150;
+            Img = new Bitmap((W + zero) * 2, H + zero);
+            W += zero;
+            using (Graphics g = Graphics.FromImage(Img))
+            {
+                Pen p = new Pen(Color.Black, 50);
+                g.DrawLine(p, W + 30, H, W + Deviation * k + 30, zero);
+                Font f = new Font("Arial", 50); ;
+                g.DrawString(((Reverse ? -1 : 1) * Deviation).ToString(), f, Brushes.Black, W + Deviation * k + 30, 40);
+                //string FromMin = (ActualValue + Min+Value/2).ToString();
+                //string ToMax = (ActualValue + Adj + Min-Value/2).ToString();
+                g.DrawString((ActualValue + Value).ToString() /*+ " (" + FromMin + " - " + ToMax + ")"*/, f, Brushes.Black, W + 60, H);
+                Point[] P = { new Point(0, zero),
+                    new Point(0, H),
+                    new Point(W + (Min + Value) * k, H),
+                    new Point(W + (Min + Value + TiltValue) * k, zero) };
+                g.FillPolygon(Brushes.LightSteelBlue, P);
+                p = new Pen(Color.Gray, 12);
+                g.DrawLine(p, W + 5, H, W + Deviation * k + 5, zero);
+                g.DrawLine(p, W + 5, H, (W - Adj * k) + 5, H);
+                g.DrawLine(p, W - Adj * k + 5, H, W + (Deviation - Adj) * k + 5, zero);
+                g.DrawLine(p, W + (Deviation - Adj) * k + 5, zero, W + Deviation * k + 5, zero);
+                g.DrawString(((Reverse ? -1 : 1) * TiltValue).ToString(), f, Brushes.Black, W + (Min + Value + TiltValue) * k - 50, 70);
+                if (Tilt) g.DrawLine(new Pen(Color.Red, 5), W + (Min + Value) * k, H, W + (Min + Value + TiltValue) * k, zero);
+            }
+
+        }
+    }
+}
